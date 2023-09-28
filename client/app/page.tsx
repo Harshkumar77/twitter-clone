3478c7a1f5e51628370f7c3c57378a46f799b9a4
volcano <harsh@noreply.com>
2023-09-28 14:50:20 +0530

    initial commit

---
+++ client/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1>Yolk</h1>
+        <h2>future</h2>
+      </main>
+    </>
+  );
+}
